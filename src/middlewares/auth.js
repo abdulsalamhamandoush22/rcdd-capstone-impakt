37c76f26110d2142e303da5cb5ff9dcdecffddcd
Abdulsalam Hamandoush <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-08-22 14:38:07 +0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -15,11 +15,7 @@ const isAuthorized = async (req, res, next) => {
     const user = await User.findById(decoded._id);
     if (!user) {
       return res.status(401).json({ msg: 'User not found.' });
-    }
-    if (req.params.userId && user._id.toString() !== req.params.userId) {
-      return res.status(403).json({ msg: 'Unauthorized access.' });
-    }
-    
+  }
     req.user = user;
     return next();
   } catch (err) {

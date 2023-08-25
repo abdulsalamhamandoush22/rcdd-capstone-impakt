17ed86c2ecf02295d9af65eae7c25aa6cc84e804
Abdulsalam Hamandoush <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-08-25 15:48:22 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const passport = require('passport');
 const swaggerUi = require('swagger-ui-express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
@@ -32,17 +30,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'assets')));
 app.use(express.static(path.join(__dirname, 'views')));
-app.use(
-  session({
-    secret: 'your-secret-key', // Replace with your actual secret key
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-// Initialize Passport and add it as middleware
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Routes
 app.use('/', userRoutes);

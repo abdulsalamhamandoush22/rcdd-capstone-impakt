15323aff7e0f4d8a973a1fccc7865afab19085ad
Abdulsalam Hamandoush <130495895+abdulsalamhamandoush22@users.noreply.github.com>
2023-08-25 15:42:03 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
 const swaggerUi = require('swagger-ui-express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
